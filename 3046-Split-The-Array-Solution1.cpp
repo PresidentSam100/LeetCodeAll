dcf39d1b8efe18a6e72aea3a3eba5f4aacd762ba
Sam Lu <pbcmarsh@gmail.com>
2024-03-07 05:10:02 -0600

    Update 3046-Split-The-Array-Solution1.cpp

--- 3046-Split-The-Array-Solution1.cpp
+++ 3046-Split-The-Array-Solution1.cpp
@@ -3,15 +3,15 @@ public:
     bool isPossibleToSplit(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int curr = 0;
-        int count = 0;
+        int count = 1;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == curr) {
                 count++;
             } else {
-                count = 0;
+                count = 1;
                 curr = nums[i];
             }
-            if (count >= 2) {
+            if (count > 2) {
                 return false;
             }
         }
