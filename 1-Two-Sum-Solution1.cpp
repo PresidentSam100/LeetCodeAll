c740fbc7d1c35bf60b65fbcfdd1872f677031b5f
Sam Lu <pbcmarsh@gmail.com>
2024-03-07 02:13:20 -0600

    Create 1-Two-Sum-Solution1.cpp

---
+++ 1-Two-Sum-Solution1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> twoSumIndices;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    twoSumIndices.push_back(i);
+                    twoSumIndices.push_back(j);
+                    break;
+                }
+            }
+        }
+        return twoSumIndices;
+    }
+};
