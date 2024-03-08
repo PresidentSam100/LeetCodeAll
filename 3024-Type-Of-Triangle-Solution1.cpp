703a5a0737f8219bbaa3312152b1e038579326aa
Sam Lu <pbcmarsh@gmail.com>
2024-03-08 06:58:22 -0600

    Update 3024-Type-Of-Triangle-Solution1.cpp

--- 3024-Type-Of-Triangle-Solution1.cpp
+++ 3024-Type-Of-Triangle-Solution1.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string triangleType(vector<int>& nums) {
         sort(nums.begin(), nums.end());
+        
         if (nums[0] + nums[1] <= nums[2]) {
             return "none";
         } else if (nums[0] == nums[1] && nums[1] == nums[2]) {
