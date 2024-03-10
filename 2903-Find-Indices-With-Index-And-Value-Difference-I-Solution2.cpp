58a194e45360b265df046ceb19406432c9165e18
Sam Lu <pbcmarsh@gmail.com>
2024-03-10 15:28:38 -0500

    Update 2903-Find-Indices-With-Index-And-Value-Difference-I-Solution2.cpp

--- 2903-Find-Indices-With-Index-And-Value-Difference-I-Solution2.cpp
+++ 2903-Find-Indices-With-Index-And-Value-Difference-I-Solution2.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
-        for (int i = 0; i + indexDifference < nums.size(); i++) {
-            for (int j = i + indexDifference; j < nums.size(); j++) {
-                if (abs(nums[i] - nums[j]) >= valueDifference) {
-                    return vector<int> {i, j};
-                }
+        int maxValue = INT_MIN;
+        int maxIndex = -1;
+        int minValue = INT_MAX;
+        int minIndex = -1;
+        for (int i = indexDifference; i < nums.size(); i++) {
+            if (nums[i - indexDifference] < minValue) {
+                minValue = nums[i - indexDifference];
+                minIndex = i - indexDifference;
+            }
+            if (nums[i - indexDifference] > maxValue) {
+                maxValue = nums[i - indexDifference];
+                maxIndex = i - indexDifference;
+            }
+            if (abs(minIndex - i) >= indexDifference && abs(minValue - nums[i]) >= valueDifference) {
+                return vector<int> {minIndex, i};
+            }
+            if (abs(maxIndex - i) >= indexDifference && abs(maxValue - nums[i]) >= valueDifference) {
+                return vector<int> {maxIndex, i};
             }
         }
+        
         return vector<int> {-1, -1};
     }
 };
