4978612e6f8b2024945652b6964831882a730dcd
Sam Lu <pbcmarsh@gmail.com>
2024-03-10 14:07:31 -0500

    Update 2903-Find-Indices-With-Index-And-Value-Difference-I-Solution2.cpp

--- 2903-Find-Indices-With-Index-And-Value-Difference-I-Solution2.cpp
+++ 2903-Find-Indices-With-Index-And-Value-Difference-I-Solution2.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i + indexDifference < nums.size(); i++) {
             for (int j = i + indexDifference; j < nums.size(); j++) {
                 if (abs(nums[i] - nums[j]) >= valueDifference) {
                     return vector<int> {i, j};
