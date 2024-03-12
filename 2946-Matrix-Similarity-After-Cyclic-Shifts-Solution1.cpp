040396387a4d8e3380ffa6dc15ff9451032e172a
Sam Lu <pbcmarsh@gmail.com>
2024-03-11 21:17:48 -0500

    Create 2946-Matrix-Similarity-After-Cyclic-Shifts-Solution1.cpp

---
+++ 2946-Matrix-Similarity-After-Cyclic-Shifts-Solution1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (mat[r][c] != mat[r][((c + ((r % 2 == 0) ? k : n-(k%n))) % n)]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
