35f1c3dd8dac420edc3e2b5a7d21eaa815372095
Sam Lu <pbcmarsh@gmail.com>
2024-04-11 15:21:40 -0500

    Create 0349-Intersection-Of-Two-Arrays-Solution2.cpp

---
+++ 0349-Intersection-Of-Two-Arrays-Solution2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> counts;
+        vector<int> intersection;
+        for (int num : nums1) {
+            counts[num]++;
+        }
+        for (int num : nums2) {
+            if (counts[num] > 0) {
+                intersection.push_back(num);
+                counts.erase(num);
+            }
+        }
+        return intersection;
+    }
+};
