6cee5913ff94e2222b067267b069800a4399bfc7
Sam Lu <pbcmarsh@gmail.com>
2024-04-11 14:28:35 -0500

    Create 0540-Single-Element-In-A-Sorted-Array-Solution1.cpp

---
+++ 0540-Single-Element-In-A-Sorted-Array-Solution1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i += 2) {
+            if (i == nums.size() - 1 || nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
